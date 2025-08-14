98f95d98a679c97131524e55d3c8dabc4ee340e0
Alex Bobowski <abobowski@govtheppl.us>
2025-08-14 16:37:04 -0700

    Improve build script, clean up server Dockerfile

--- backend/server/build.sh
+++ backend/server/build.sh
@@ -4,14 +4,27 @@
 # Check if npm exists, install if not
 dpkg -s python3-pip >> /dev/null
 if [ $? -ne 0 ]; then
-	apt install -y python3-pip
+	echo "python3-pip is required"
 fi
 
 # Check if openapi-generator-cli is available, install if not
 if ! command -v openapi-generator-cli >/dev/null 2>&1; then
-	pip install openapi-generator-cli
+	echo "openapi generator is required, install with:"
+	echo "	pip install openapi-generator-cli"
 fi
 
 openapi-generator-cli generate -i ../../docs/api.yaml -g python-flask -o ./generated -c openapi-config.json
+if [ $? -ne 0 ]; then
+	echo "Generation Failed"
+	exit 1
+fi
+echo "Generation completed successfully"
+
 cp ./controllers/* generated/candid/controllers/
-docker build -t candid ./generated/
+echo "Copied controllers from ./controllers/"
+
+echo "Build and run the container:"
+echo "	sudo docker build -t candid ./generated/ && sudo docker run -p 8080:8080 candid"
+echo ""
+echo "Then access the API UI:"
+echo "	http://127.0.0.1:8080/api/v1/ui"
